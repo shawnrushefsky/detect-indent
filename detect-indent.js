4f28fb69a2c50246d60737a55c3a0de69ab59c53
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2020-12-17 14:28:44 -0600

    formatting

--- detect-indent.js
+++ detect-indent.js
@@ -1,17 +1,27 @@
+/**
+ * Takes in a JSON file, and determines it's indentation
+ * @param {string} file
+ *
+ * @returns {{
+ * amount: number,
+ * type: ( 'spaces' | 'tabs' ),
+ * indent: string
+ * }}
+ */
 function detectIndentation(file) {
-  const lines = file.split('\n');
+  const lines = file.split("\n");
   const tokenTypes = {
     spaces: 0,
-    tabs: 0
+    tabs: 0,
   };
   const numIndentation = [];
 
   lines.forEach((line) => {
     for (let i = 0; i < line.length; i++) {
       const char = line[i];
-      if (char === ' ') {
+      if (char === " ") {
         tokenTypes.spaces += 1;
-      } else if (char === '\t') {
+      } else if (char === "\t") {
         tokenTypes.tabs += 1;
       } else {
         numIndentation.push(i);
@@ -22,22 +32,24 @@ function detectIndentation(file) {
 
   const differences = [];
   for (let i = 1; i < numIndentation.length; i++) {
-    if (numIndentation[i] > numIndentation[i-1]) {
-      differences.push(numIndentation[i] - numIndentation[i-1]);
+    if (numIndentation[i] > numIndentation[i - 1]) {
+      differences.push(numIndentation[i] - numIndentation[i - 1]);
     }
   }
 
-  function _mode(arr){
-    return arr.sort((a,b) =>
-          arr.filter(v => v===a).length
-        - arr.filter(v => v===b).length
-    ).pop();
+  function _mode(arr) {
+    return arr
+      .sort(
+        (a, b) =>
+          arr.filter((v) => v === a).length - arr.filter((v) => v === b).length
+      )
+      .pop();
   }
 
-  const type = tokenTypes.spaces > tokenTypes.tabs ? 'spaces' : 'tabs';
+  const type = tokenTypes.spaces > tokenTypes.tabs ? "spaces" : "tabs";
   const characters = {
-    'spaces': ' ',
-    'tabs': '\t'
+    spaces: " ",
+    tabs: "\t",
   };
 
   const amount = _mode(differences);
@@ -45,8 +57,8 @@ function detectIndentation(file) {
   return {
     amount,
     type,
-    indent: characters[type].repeat(amount)
-  }
+    indent: characters[type].repeat(amount),
+  };
 }
 
 module.exports = detectIndentation;
--- test/detect-indent.js
+++ test/detect-indent.js
@@ -1,47 +1,59 @@
-const { expect } = require('chai');
-const fs = require('fs');
-const path = require('path');
-const detectIndent = require('../detect-indent');
+const { expect } = require("chai");
+const fs = require("fs");
+const path = require("path");
+const detectIndent = require("../detect-indent");
 
-const spaces1 = fs.readFileSync(path.join(__dirname, 'fixtures/1space.json'), 'utf8');
-const spaces2 = fs.readFileSync(path.join(__dirname, 'fixtures/2space.json'), 'utf8');
-const spaces4 = fs.readFileSync(path.join(__dirname, 'fixtures/4space.json'), 'utf8');
-const tabs1 = fs.readFileSync(path.join(__dirname, 'fixtures/1tab.json'), 'utf8');
+const spaces1 = fs.readFileSync(
+  path.join(__dirname, "fixtures/1space.json"),
+  "utf8"
+);
+const spaces2 = fs.readFileSync(
+  path.join(__dirname, "fixtures/2space.json"),
+  "utf8"
+);
+const spaces4 = fs.readFileSync(
+  path.join(__dirname, "fixtures/4space.json"),
+  "utf8"
+);
+const tabs1 = fs.readFileSync(
+  path.join(__dirname, "fixtures/1tab.json"),
+  "utf8"
+);
 
-describe('detect-indent', () => {
-  it('works with 1 space', () => {
+describe("detect-indent", () => {
+  it("works with 1 space", () => {
     const result = detectIndent(spaces1);
     expect(result).to.deep.equal({
       amount: 1,
-      type: 'spaces',
-      indent: ' '
+      type: "spaces",
+      indent: " ",
     });
   });
 
-  it('works with 2 spaces', () => {
+  it("works with 2 spaces", () => {
     const result = detectIndent(spaces2);
     expect(result).to.deep.equal({
       amount: 2,
-      type: 'spaces',
-      indent: '  '
+      type: "spaces",
+      indent: "  ",
     });
   });
 
-  it('works with 4 spaces', () => {
+  it("works with 4 spaces", () => {
     const result = detectIndent(spaces4);
     expect(result).to.deep.equal({
       amount: 4,
-      type: 'spaces',
-      indent: '    '
+      type: "spaces",
+      indent: "    ",
     });
   });
 
-  it('works with 1 tab', () => {
+  it("works with 1 tab", () => {
     const result = detectIndent(tabs1);
     expect(result).to.deep.equal({
       amount: 1,
-      type: 'tabs',
-      indent: '\t'
+      type: "tabs",
+      indent: "\t",
     });
   });
 });
