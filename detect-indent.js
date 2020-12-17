29570855b915097380ffb49b9188346f5c4c5497
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2020-12-17 14:50:23 -0600

    simplify

--- detect-indent.js
+++ detect-indent.js
@@ -47,17 +47,13 @@ function detectIndentation(file) {
   }
 
   const type = tokenTypes.spaces > tokenTypes.tabs ? "spaces" : "tabs";
-  const characters = {
-    spaces: " ",
-    tabs: "\t",
-  };
-
+  const character = type === 'spaces' ? ' ' : '\t';
   const amount = _mode(differences);
 
   return {
     amount,
     type,
-    indent: characters[type].repeat(amount),
+    indent: character.repeat(amount),
   };
 }
 
